e9c3663c9d9d68c4b6e5e9800d9efe5331df3342
dpena <dpena@rent.com>
2013-08-28 12:24:19 -0700

    Use thoughtbot install code

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#
-# vim
-#
-
-# git autocomplete
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-
-# add git branch to prompt
-curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u ~/.vimrc.bundles +BundleInstall +qa
