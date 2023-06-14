d1fd405fdce5ce18076e8a5c2f579f12f0fc3e43
Dakotah Peña <dpena@usertesting.com>
2023-06-14 14:11:54 -0700

    style(macos-setup): added echo statements
    wasn't clear if it had run or not

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,5 +1,6 @@
 # Sensible defaults for tweaking MacOS here and there.
-#
+
+echo "Setting some MacOS preferences…"
 
 # Make key repeat speed much faster
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
@@ -7,3 +8,5 @@ defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
 # Disable the press and hold functionality for diacritics characters
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+echo "Done!"
