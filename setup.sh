2befeae5e442300de51dbd69d34f1689b2bf522d
DP <git@dakotahpena.dev>
2024-01-27 03:59:11 +0000

    Update thoughtbot debian repo

--- setup.sh
+++ setup.sh
@@ -44,8 +44,8 @@ install_rcm () {
 		brew tap thoughtbot/formulae
 		brew install rcm
 	else
-		wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
-		echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+		sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
+    echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 		sudo apt-get update
 		sudo apt-get -y install rcm
 	fi
